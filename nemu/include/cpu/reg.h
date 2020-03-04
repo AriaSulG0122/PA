25730adaa6029c8a317f47c449014a97cb59a00d
tracer-ics2017 <tracer@njuics.org>
2020-03-04 19:14:44 +0800

    >  compile 1410576 llt Linux ubuntu 4.15.0-88-generic #88~16.04.1-Ubuntu SMP Wed Feb 12 04:18:19 UTC 2020 i686 i686 i686 GNU/Linux  19:14:44 up  8:53,  3 users,  load average: 0.08, 0.02, 0.01 3280673f73ad3c3860cf99dba0afd91142709702

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -4,14 +4,14 @@
 #include "common.h"
 
 //***declare 32bit Regs.
-//enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };
-enum { R_EAX, R_EBX, R_ECX, R_EDX, R_ESP, R_EBP, R_ESI, R_EDI };
+enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };
+//enum { R_EAX, R_EBX, R_ECX, R_EDX, R_ESP, R_EBP, R_ESI, R_EDI };
 //***declare 16bit Regs.
-//enum { R_AX, R_CX, R_DX, R_BX, R_SP, R_BP, R_SI, R_DI };
-enum { R_AX, R_BX, R_CX, R_DX, R_SP, R_BP, R_SI, R_DI };
+enum { R_AX, R_CX, R_DX, R_BX, R_SP, R_BP, R_SI, R_DI };
+//enum { R_AX, R_BX, R_CX, R_DX, R_SP, R_BP, R_SI, R_DI };
 //***declare 8bit Regs.
-//enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
-enum { R_AH, R_AL, R_BH, R_BL, R_CH, R_CL, R_DH, R_DL };
+enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
+//enum { R_AH, R_AL, R_BH, R_BL, R_CH, R_CL, R_DH, R_DL };
 
 /* TODO: Re-organize the `CPU_state' structure to match the register
  * encoding scheme in i386 instruction format. For example, if we
@@ -34,8 +34,8 @@ typedef struct {
     } gpr[8];
 
     struct{
-      //rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-      rtlreg_t eax, ebx, ecx, edx, esp, ebp, esi, edi;
+      rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+      //rtlreg_t eax, ebx, ecx, edx, esp, ebp, esi, edi;
     };
   };
   vaddr_t eip;
