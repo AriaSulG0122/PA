91ffaf8d54455816b9a4dec73013be8be198c07b
tracer-ics2017 <tracer@njuics.org>
2020-04-11 18:04:39 +0800

    >  compile 1410576 llt Linux ubuntu 4.15.0-88-generic #88~16.04.1-Ubuntu SMP Wed Feb 12 04:18:19 UTC 2020 i686 i686 i686 GNU/Linux  18:04:39 up  5:58,  3 users,  load average: 0.13, 0.06, 0.01 f36de54c80deebf4d5bb43d8108652d65da10b28

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -34,8 +34,7 @@ make_EHelper(call) {
 
 make_EHelper(ret) {
   //TODO();
-  rtl_pop(&t0);//取出栈顶保存的eip值，然后将其设置为跳转的eip值
-  decoding.jmp_eip=t0;
+  rtl_pop(&decoding.jmp_eip);//取出栈顶保存的eip值，然后将其设置为跳转的eip值
   decoding.is_jmp=1;//指令跳转为真
   print_asm("ret");
 }
