b0403ebaed7557cc3121343c48d66c2f2c7188a6
tracer-ics2017 <tracer@njuics.org>
2020-04-23 00:06:56 +0800

    >  compile 1410576 llt Linux ubuntu 4.15.0-96-generic #97~16.04.1-Ubuntu SMP Wed Apr 1 03:01:48 UTC 2020 i686 i686 i686 GNU/Linux  00:06:56 up  4:25,  1 user,  load average: 0.08, 0.02, 0.01 dab1c1ec59ac4b50406715872d1ea23e83ccf47c

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -12,6 +12,7 @@ make_EHelper(call);//call为跳转控制类指令
 make_EHelper(ret);//过程回归
 make_EHelper(jcc);//Jump if Condition is met
 make_EHelper(jmp);//跳转
+make_EHelper(jmp_rm);
 //data-mov.c
 make_EHelper(pop);//将某个值从栈顶取出
 make_EHelper(push);//将某个值放入到栈顶
