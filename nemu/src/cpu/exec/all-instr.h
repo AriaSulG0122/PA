51ea53e09359e2894dcedf6f39ed6a8ab8f659d1
tracer-ics2017 <tracer@njuics.org>
2020-04-22 22:39:03 +0800

    >  compile 1410576 llt Linux ubuntu 4.15.0-96-generic #97~16.04.1-Ubuntu SMP Wed Apr 1 03:01:48 UTC 2020 i686 i686 i686 GNU/Linux  22:39:03 up  2:57,  1 user,  load average: 0.01, 0.01, 0.00 2e3def5eaa064d1bd30831f6786ebe5f32cf0bd6

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -21,6 +21,8 @@ make_EHelper(sub); //减法
 make_EHelper(add); //加法
 make_EHelper(adc); //带CF加法
 make_EHelper(cmp); //Compare Two Operand  
+make_EHelper(inc); //+1
+make_EHelper(dec); //-1
 //logic.c   
 make_EHelper(xor);//异或
 make_EHelper(and);//与
