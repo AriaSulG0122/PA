f8f7dfba7efbab444ff900f984e3f7583b860566
tracer-ics2017 <tracer@njuics.org>
2020-04-24 22:03:44 +0800

    >  run 1410576 llt Linux ubuntu 4.15.0-96-generic #97~16.04.1-Ubuntu SMP Wed Apr 1 03:01:48 UTC 2020 i686 i686 i686 GNU/Linux  22:03:44 up  7:28,  1 user,  load average: 0.16, 0.06, 0.02 1321c7e296c4e57f537f87d9dea310724d8c3bb

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -11,8 +11,8 @@ void _ioe_init() {
 
 //返回系统启动后经过的毫秒数
 unsigned long _uptime() {
-  //return 0;
-  return inl(RTC_PORT)-boot_time;//毫秒数就是当前时间和建立时间的差值
+  return 0;
+  //return inl(RTC_PORT)-boot_time;//毫秒数就是当前时间和建立时间的差值
 }
 
 uint32_t* const fb = (uint32_t *)0x40000;
