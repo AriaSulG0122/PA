c04486af82b8f8564ed723ad926369ca6375f3e5
tracer-ics2017 <tracer@njuics.org>
2020-04-25 17:22:00 +0800

    >  run 1410576 llt Linux ubuntu 4.15.0-96-generic #97~16.04.1-Ubuntu SMP Wed Apr 1 03:01:48 UTC 2020 i686 i686 i686 GNU/Linux  17:22:00 up 12:14,  1 user,  load average: 0.05, 0.30, 0.17 af90d23bab806b1a14aefcc46a9ec933e612eec6

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -27,13 +27,13 @@ extern void* memcpy(void *, const void *, int);
 
 //用于将pixels指定的矩形像素绘制到屏幕中以(x,y)和(x+w,y+h)两点连线为对角线的矩形区域
 void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
-  int i;
-  for (i = 0; i < _screen.width * _screen.height; i++) {
-    fb[i] = pixels[i];
-  }
   // int i;
-  // for(i=0;i<h;i++)
-  //     memcpy(fb+(y+i)*_screen.width+x, pixels+i*w, w*4);
+  // for (i = 0; i < _screen.width * _screen.height; i++) {
+  //   fb[i] = i;
+  // }
+  int i;
+  for(i=0;i<h;i++)
+      memcpy(fb+(y+i)*_screen.width+x, pixels+i*w, w*4);
 }
 
 //用于将之前的绘制内容同步到屏幕上（在NEMU中绘制内容总是会同步到屏幕上，因而无需实现此API）
