beda24bf6cb4f56620cab91ea01036de66f57a91
tracer-ics2017 <tracer@njuics.org>
2020-05-06 23:18:11 +0800

    >  compile 1410576 llt Linux ubuntu 4.15.0-96-generic #97~16.04.1-Ubuntu SMP Wed Apr 1 03:01:48 UTC 2020 i686 i686 i686 GNU/Linux  23:18:11 up  4:38,  3 users,  load average: 0.08, 0.03, 0.04 8474f1b7c05870da95d99fc7f84f9f448ca373b3

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -148,7 +148,7 @@ static inline void rtl_not(rtlreg_t* dest) {
   *dest = ~(*dest);
 }
 //符号拓展
-void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
+static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
   //TODO();
   rtl_li(&t3,32-width*8);//t2为多余位
