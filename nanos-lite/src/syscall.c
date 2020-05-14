afb150981e22109ce7ac44692e5b444ef57c03d8
tracer-ics2017 <tracer@njuics.org>
2020-05-14 11:20:21 +0800

    >  run 1410576 llt Linux ubuntu 4.15.0-96-generic #97~16.04.1-Ubuntu SMP Wed Apr 1 03:01:48 UTC 2020 i686 i686 i686 GNU/Linux  11:20:21 up 10:15,  2 users,  load average: 0.08, 0.02, 0.01 5a12a9e0424a21f2677ca61d1bc3f43a9f6f7276

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,21 @@
 #include "common.h"
 #include "syscall.h"
 
+static inline _RegSet* sys_write(_RegSet *r){
+  int fd=(int)SYSCALL_ARG2(r);
+  char *buf=(char*)SYSCALL_ARG3(r);
+  int len=(int)SYSCALL_ARG4(r);
+  if(fd==1||fd==2){//stdout或stderr
+    for(int i=0;i<len;i++){
+      //将buf为首地址的len字节输出到串口
+      _putc(buf[i]);
+    }
+    //设置返回值
+    SYSCALL_ARG1(r)=SYSCALL_ARG4(r);
+  }
+  return NULL;
+}
+
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
@@ -14,6 +29,8 @@ _RegSet* do_syscall(_RegSet *r) {
     case SYS_exit:
       _halt(a[1]);
       break;
+    case SYS_write:
+      return sys_write(r);
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
   return NULL;
