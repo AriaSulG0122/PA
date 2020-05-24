6340e772ec2d6b8846c5685ee53e35fc4fdd8d52
tracer-ics2017 <tracer@njuics.org>
2020-05-24 20:11:14 +0800

    >  run 1410576 llt Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 i686 i686 GNU/Linux  20:11:14 up  1:48,  1 user,  load average: 0.24, 0.34, 0.21 f9f5c518515fdd71ecffcc1fe496575a71540662

--- nanos-lite/include/common.h
+++ nanos-lite/include/common.h
@@ -9,6 +9,4 @@ typedef char bool;
 #define true 1
 #define false 0
 
-_RegSet* do_syscall(_RegSet *r);
-
 #endif
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,62 +1,70 @@
 #include "common.h"
 #include "syscall.h"
 
-extern int fs_open(const char *pathname,int flags,int mode);
-extern ssize_t fs_read(int fd,void *buf,size_t len);
-extern ssize_t fs_write(int fd,const void *buf,size_t len);
+extern char _end;
+extern ssize_t fs_read(int fd, void *buf, size_t len);
+extern ssize_t fs_write(int fd, const void *buf, size_t len);
+extern int fs_open(const char *pathname, int flags, int mode);
+extern off_t fs_lseek(int fd, off_t offset, int whence);
 extern int fs_close(int fd);
-extern size_t fs_filesz(int fd);
-extern off_t fs_lseek(int fd,off_t offset,int whence);
 
-static inline _RegSet* sys_write(_RegSet *r){
-  int fd=(int)SYSCALL_ARG2(r);
-  char *buf=(char*)SYSCALL_ARG3(r);
-  int len=(int)SYSCALL_ARG4(r);
-  Log("fd:%d,char:%c,len:%d",fd,*buf,len);
-  if(fd==1||fd==2){//stdout或stderr
-    for(int i=0;i<len;i++){
-      //将buf为首地址的len字节输出到串口
-      _putc(buf[i]);
-    }
-    //设置返回值
-    SYSCALL_ARG1(r)=SYSCALL_ARG4(r);
-  }
-  return NULL;
+uintptr_t sys_write(int fd, const void *buf, size_t count) {
+	uintptr_t i = 0;
+	if (fd == 1 || fd == 2) {
+		for(; count > 0; count--) {
+			_putc(((char*)buf)[i]);
+			i++;
+		}
+	}
+	return i;
 }
 
+//处理函数
 _RegSet* do_syscall(_RegSet *r) {
-  uintptr_t a[4];
+
+  uintptr_t a[4], neweax = -1;
   a[0] = SYSCALL_ARG1(r);
   a[1] = SYSCALL_ARG2(r);
   a[2] = SYSCALL_ARG3(r);
   a[3] = SYSCALL_ARG4(r);
-  switch (a[0]) {
-    case SYS_none:
-      r->eax = 1;
-      break;
+
+  switch (a[0]) {		
+	case SYS_none:
+		neweax = 1;
+		break;
+
     case SYS_exit:
-      _halt(a[1]);
-      break;
-    case SYS_write:
-      //return sys_write(r);
-      r->eax=fs_write(a[1],(void *)a[2],a[3]);
-      break;
-    case SYS_brk:
-      r->eax=0;
-      break;
-    case SYS_read:
-      r->eax=fs_read(a[1],(void *)a[2],a[3]);
-      break;
-    case SYS_open:
-      r->eax=fs_open((char *)a[1],a[2],a[3]);
-      break;
-    case SYS_close:
-      r->eax=fs_close(a[1]);
-      break;
-    case SYS_lseek:
-      r->eax=fs_lseek(a[1],a[2],a[3]);
-      break;
+		_halt(a[1]);
+		break;	
+		
+	case SYS_write:
+		//neweax = sys_write(a[1], (void *)a[2], a[3]);
+		neweax = fs_write(a[1], (void *)a[2], a[3]);
+		break;
+
+	case SYS_brk:
+		neweax = 0;
+		break;
+
+	case SYS_read:
+		neweax = fs_read(a[1], (void *)a[2], a[3]);
+		break;
+
+	case SYS_open:
+		neweax = fs_open((char *)a[1], a[2], a[3]);
+		break;
+
+	case SYS_close:
+		neweax = fs_close(a[1]);
+		break;
+
+	case SYS_lseek:
+		neweax = fs_lseek(a[1], a[2], a[3]);
+		break;
+
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+
+  SYSCALL_ARG1(r) = neweax;
   return NULL;
 }
