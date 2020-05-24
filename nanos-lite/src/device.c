cf1cd3c3aa71e8ba1b0f4a25394d960f3300e73d
tracer-ics2017 <tracer@njuics.org>
2020-05-24 21:29:15 +0800

    >  run 1410576 llt Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 i686 i686 GNU/Linux  21:29:15 up  3:06,  1 user,  load average: 0.20, 0.05, 0.05 3911626e1e75acd9d6abb692e6448a40f17041e5

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -21,10 +21,10 @@ size_t events_read(void *buf, size_t len)
   
   if(key!=_KEY_NONE){//没有按键，则表明为时钟事件
     sprintf(str,"%s %s\n",down?"kd":"ku",keyname[key]);
-    Log("Key:%s\n",keyname[key]);
+    //Log("Key:%s\n",keyname[key]);
   }else{
     sprintf(str,"t %d\n",_uptime());
-    Log("Time\n");
+    //Log("Time\n");
   }
   if(strlen(str)<=len){
     strncpy((char*)buf,str,strlen(str));
