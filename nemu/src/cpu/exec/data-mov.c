4fb2a74cae3044c97a94b953efafd7a8f49ebc8c
tracer-ics2017 <tracer@njuics.org>
2020-05-24 21:09:01 +0800

    >  compile 1410576 llt Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 i686 i686 GNU/Linux  21:09:01 up  2:46,  1 user,  load average: 0.24, 0.12, 0.14 39e0c95e99c43c182b60805ec6497f6aabd34856

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -7,57 +7,27 @@ make_EHelper(mov) {
 
 make_EHelper(push) {
   //TODO();
+  //rtl_push(&id_dest->val);//把value值push到栈顶
+  if (id_dest->width == 1) {
+    id_dest->val = (int32_t)(int8_t)id_dest->val;
+  }
   rtl_push(&id_dest->val);
-
   print_asm_template1(push);
 }
 
 make_EHelper(pop) {
   //TODO();
+  //rtl_pop(&id_dest->val);//不能这么写！
+  /* 调用rtl_pop()取出一个32位值到临时寄存器 */
   rtl_pop(&t2);
+  /* 使用operand_write()将取出的临时值写入目标寄存器 */
   operand_write(id_dest,&t2);
   print_asm_template1(pop);
 }
 
+//把通用寄存器的值压入堆栈
 make_EHelper(pusha) {
   //TODO();
-  /*if(decoding.is_operand_size_16){
-    t0=cpu.esp;
-    rtl_lr_w(&t1,R_AX);
-    rtl_push(&t1);
-    rtl_lr_w(&t1,R_CX);
-    rtl_push(&t1);
-    rtl_lr_w(&t1,R_DX);
-    rtl_push(&t1);
-    rtl_lr_w(&t1,R_BX);
-    rtl_push(&t1);
-    rtl_push(&t0);
-    rtl_lr_w(&t1,R_BP);
-    rtl_push(&t1);
-    rtl_lr_w(&t1,R_SI);
-    rtl_push(&t1);
-    rtl_lr_w(&t1,R_DI);
-    rtl_push(&t1);
-  }
-  else{
-    t0=cpu.esp;
-    rtl_lr_w(&t1,R_EAX);
-    rtl_push(&t1);
-    rtl_lr_w(&t1,R_ECX);
-    rtl_push(&t1);
-    rtl_lr_w(&t1,R_EDX);
-    rtl_push(&t1);
-    rtl_lr_w(&t1,R_EBX);
-    rtl_push(&t1);
-    rtl_push(&t0);
-    rtl_lr_w(&t1,R_EBP);
-    rtl_push(&t1);
-    rtl_lr_w(&t1,R_ESI);
-    rtl_push(&t1);
-    rtl_lr_w(&t1,R_EDI);
-    rtl_push(&t1);
-  }*/
-
   t0=cpu.esp;
   rtl_push(&cpu.eax);
   rtl_push(&cpu.ecx);
@@ -67,139 +37,57 @@ make_EHelper(pusha) {
   rtl_push(&cpu.ebp);
   rtl_push(&cpu.esi);
   rtl_push(&cpu.edi);
-
   print_asm("pusha");
 }
 
 make_EHelper(popa) {
   //TODO();
-/*
-  if(decoding.is_operand_size_16){
-    rtl_pop(&t1);
-    rtl_sr_w(R_DI,&t1);
-    rtl_pop(&t1);
-    rtl_sr_w(R_SI,&t1);
-    rtl_pop(&t1);
-    rtl_sr_w(R_BP,&t1);
-    cpu.esp+=4;
-    rtl_pop(&t1);
-    rtl_sr_w(R_BX,&t1);
-    rtl_pop(&t1);
-    rtl_sr_w(R_DX,&t1);
-    rtl_pop(&t1);
-    rtl_sr_w(R_CX,&t1);
-    rtl_pop(&t1);
-    rtl_sr_w(R_AX,&t1);
-  }
-  else{
-    rtl_pop(&t1);
-    rtl_sr_w(R_EDI,&t1);
-    rtl_pop(&t1);
-    rtl_sr_w(R_ESI,&t1);
-    rtl_pop(&t1);
-    rtl_sr_w(R_EBP,&t1);
-    cpu.esp+=4;
-    rtl_pop(&t1);
-    rtl_sr_w(R_EBX,&t1);
-    rtl_pop(&t1);
-    rtl_sr_w(R_EDX,&t1);
-    rtl_pop(&t1);
-    rtl_sr_w(R_ECX,&t1);
-    rtl_pop(&t1);
-    rtl_sr_w(R_EAX,&t1);
-  }
-*/
-
   rtl_pop(&cpu.edi);
   rtl_pop(&cpu.esi);
   rtl_pop(&cpu.ebp);
-  rtl_pop(&t0);
+  rtl_pop(&t0);//Skip ESP
   rtl_pop(&cpu.ebx);
   rtl_pop(&cpu.edx);
   rtl_pop(&cpu.ecx);
   rtl_pop(&cpu.eax);
-
-
-
   print_asm("popa");
 }
 
 make_EHelper(leave) {
   //TODO();
-  rtl_mv(&cpu.esp,&cpu.ebp);
-  rtl_pop(&cpu.ebp);
+  rtl_mv(&cpu.esp,&cpu.ebp);//Set ESP to EBP
+  rtl_pop(&cpu.ebp);//pop EBP
   print_asm("leave");
 }
 
 make_EHelper(cltd) {
-/*
-  if (decoding.is_operand_size_16) {//ax
+  if (decoding.is_operand_size_16) {//CWD instruction，DX:AX(sign-extend of AX)
     //TODO();
-    rtl_lr_w(&t0,R_AX);
-    rtl_sext(&t0,&t0,2);
-    if(t0<0)
-    {
-	reg_w(R_DX)=0xffff;
-    }
-    else
-    {
-	reg_w(R_DX)=0;
-    }
+    rtl_msb(&t0,&cpu.eax,2);//获取16位数的最高位，看看是否<0，即获取AX的最高位
+    if(t0 == 1)cpu.edx = cpu.edx | 0xffff;//AX<0，则以AX的符号位拓展DX
+    else cpu.edx = 0;
   }
-  else {//eax
+  else {//CDQ instruction,EDX:EAX(sign-extend of EAX)
     //TODO();
-    rtl_lr_l(&t0,R_EAX);
-    if((int32_t)t0<0)
-    {
-	reg_l(R_EDX)=0xffffffff;
-    }
-    else
-    {
-	reg_l(R_EDX)=0;
-    }
+    rtl_msb(&t0,&cpu.eax,4);
+    if(t0 == 1)cpu.edx = cpu.edx | 0xffffffff;
+    else cpu.edx = 0;
   }
-*/
-  if(decoding.is_operand_size_16)
-  {
-    rtl_lr_w(&t0,R_AX);
-    rtl_sext(&t0,&t0,2);
-    rtl_sari(&t0,&t0,31);
-    rtl_sr_w(R_DX,&t0);
-  }
-  else
-  {
-    rtl_sari(&cpu.edx,&cpu.eax,31);
-  }
-
-
   print_asm(decoding.is_operand_size_16 ? "cwtl" : "cltd");
 }
 
-make_EHelper(cwtl) {
-/*
-  if (decoding.is_operand_size_16) {
+make_EHelper(cwtl) {//Convert Byte to Word/Convert Word to Doubleword
+  if (decoding.is_operand_size_16) {//AX<-SignExtend(AL),Convert Byte to Word
     //TODO();
-    rtl_lr_b(&t0,R_AL);
-    rtl_sext(&t0,&t0,1);//al->ax
-    reg_w(R_AX)=t0;
+    rtl_sext(&t0,&cpu.eax,1);//进行符号拓展
+    cpu.eax = (cpu.eax & 0xffff0000) | (t0 & 0xffff);
   }
-  else {
+  else {//EAX<-SignExtend(AX),Convert Word to Doubleword
     //TODO();
-    rtl_lr_w(&t0,R_AX);
-    rtl_sext(&t0,&t0,2);//ax->aex
-    reg_l(R_EAX)=t0;
-  }*/
+    rtl_sext(&t0,&cpu.eax,2);
+    cpu.eax = t0;
+  }
 
-  if(decoding.is_operand_size_16)
-  {
-    panic("operand size shoule be 32");
-  }
-  else
-  {
-    rtl_lr_w(&t0,R_AX);
-    rtl_sext(&t0,&t0,2);
-    rtl_sr_l(R_EAX,&t0);
-  }
   print_asm(decoding.is_operand_size_16 ? "cbtw" : "cwtl");
 }
 
--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -1,10 +1,11 @@
 #include "cpu/exec.h"
 #include "all-instr.h"
 
+//decode记录译码函数，execute为执行函数，width为宽度信息
 typedef struct {
-  DHelper decode;
-  EHelper execute;
-  int width;
+  DHelper decode;   //译码函数
+  EHelper execute;  //执行函数
+  int width;        //操作数宽度信息
 } opcode_entry;
 
 #define IDEXW(id, ex, w)   {concat(decode_, id), concat(exec_, ex), w}
@@ -13,9 +14,10 @@ typedef struct {
 #define EX(ex)             EXW(ex, 0)
 #define EMPTY              EX(inv)
 
+//将操作数的宽度信息记录在全局译码信息decoding中
 static inline void set_width(int width) {
-  if (width == 0) {
-    width = decoding.is_operand_size_16 ? 2 : 4;
+  if (width == 0) {//若width等于零，进行判断
+    width = decoding.is_operand_size_16 ? 2 : 4;//如果operand_size_16，则width为2;否则width为4，即32位
   }
   decoding.src.width = decoding.dest.width = decoding.src2.width = width;
 }
@@ -24,12 +26,14 @@ static inline void set_width(int width) {
 static inline void idex(vaddr_t *eip, opcode_entry *e) {
   /* eip is pointing to the byte next to opcode */
   if (e->decode)
-    e->decode(eip);
-  e->execute(eip);
+    e->decode(eip);//译码
+    e->execute(eip);//执行
 }
 
 static make_EHelper(2byte_esc);
 
+//声明一组opcode_table_gp[x] [8]，每一个元素都包含函数opcode_table_gp[x] [i]，该函数最终落实到IDEXW(id,ex,w)
+/*idex一句将根据decoding的ext_opcode来确定执行哪个函数*/
 #define make_group(name, item0, item1, item2, item3, item4, item5, item6, item7) \
   static opcode_entry concat(opcode_table_, name) [8] = { \
     /* 0x00 */	item0, item1, item2, item3, \
@@ -56,13 +60,13 @@ make_group(gp3,
 
   /* 0xfe */
 make_group(gp4,
-    EX(inc), EX(dec), EMPTY, EMPTY,
+    EMPTY, EXW(dec,1), EMPTY, EMPTY,
     EMPTY, EMPTY, EMPTY, EMPTY)
 
   /* 0xff */
 make_group(gp5,
     EX(inc), EX(dec), EX(call_rm), EX(call),
-    EX(jmp_rm), EX(jmp), EX(push), EMPTY)
+    EX(jmp_rm), EMPTY, EX(push), EMPTY)
 
   /* 0x0f 0x01*/
 make_group(gp7,
@@ -70,10 +74,12 @@ make_group(gp7,
     EMPTY, EMPTY, EMPTY, EMPTY)
 
 /* TODO: Add more instructions!!! */
-
+//***Decoding lookup table
+//全局译码表
+//每一个 opcode 对应相应指令的译码函数,执行函数,以及操作数宽度
 opcode_entry opcode_table [512] = {
-  /* 0x00 */	IDEXW(G2E,add,1), IDEX(G2E,add), IDEXW(E2G,add,1), IDEX(E2G,add),
-  /* 0x04 */	IDEXW(I2a,add,1), IDEX(I2a,add), EMPTY, EMPTY,
+  /* 0x00 */	IDEXW(G2E,add,1), IDEX(G2E,add),IDEX(E2G,add),IDEX(E2G,add),
+  /* 0x04 */	IDEXW(I2a,add,1), IDEX(I2a,add),EMPTY, EMPTY,
   /* 0x08 */	IDEXW(G2E,or,1), IDEX(G2E,or), IDEXW(E2G,or,1), IDEX(E2G,or),
   /* 0x0c */	IDEXW(I2a,or,1), IDEX(I2a,or), EMPTY, EX(2byte_esc),
   /* 0x10 */	IDEXW(G2E,adc,1), IDEX(G2E,adc), IDEXW(E2G,adc,1), IDEX(E2G,adc),
@@ -82,29 +88,29 @@ opcode_entry opcode_table [512] = {
   /* 0x1c */	IDEXW(I2a,sbb,1), IDEX(I2a,sbb), EMPTY, EMPTY,
   /* 0x20 */	IDEXW(G2E,and,1), IDEX(G2E,and), IDEXW(E2G,and,1), IDEX(E2G,and),
   /* 0x24 */	IDEXW(I2a,and,1), IDEX(I2a,and), EMPTY, EMPTY,
-  /* 0x28 */	IDEXW(G2E,sub,1), IDEX(G2E,sub), IDEXW(E2G,sub,1), IDEX(E2G,sub),
+  /* 0x28 */	IDEXW(G2E,sub,1), IDEX(G2E,sub), IDEXW(E2G,sub,1), IDEX(E2G,sub), 
   /* 0x2c */	IDEXW(I2a,sub,1), IDEX(I2a,sub), EMPTY, EMPTY,
   /* 0x30 */	IDEXW(G2E,xor,1), IDEX(G2E,xor), IDEXW(E2G,xor,1), IDEX(E2G,xor),
-  /* 0x34 */	EMPTY, IDEX(I2a,xor), EMPTY, EMPTY,
+  /* 0x34 */	IDEXW(I2a,xor,1), IDEX(I2a,xor), EMPTY, EMPTY,
   /* 0x38 */	IDEXW(G2E,cmp,1), IDEX(G2E,cmp), IDEXW(E2G,cmp,1), IDEX(E2G,cmp),
   /* 0x3c */	IDEXW(I2a,cmp,1), IDEX(I2a,cmp), EMPTY, EMPTY,
-  /* 0x40 */	IDEX(r,inc), IDEX(r,inc), IDEX(r,inc), IDEX(r,inc),
-  /* 0x44 */	EMPTY, EMPTY, IDEX(r,inc), IDEX(r,inc),
-  /* 0x48 */	IDEX(r,dec), IDEX(r,dec), IDEX(r,dec), IDEX(r,dec),
-  /* 0x4c */	EMPTY, EMPTY, IDEX(r,dec), IDEX(r,dec),
-  /* 0x50 */	IDEX(r,push), IDEX(r,push), IDEX(r,push), IDEX(r,push),
-  /* 0x54 */	IDEX(r,push), IDEX(r,push), IDEX(r,push), IDEX(r,push),
+  /* 0x40 */	IDEX(r,inc), IDEX(r,inc), IDEX(r,inc), IDEX(r,inc), 
+  /* 0x44 */	IDEX(r,inc), IDEX(r,inc), IDEX(r,inc), IDEX(r,inc), 
+  /* 0x48 */	IDEX(r,dec),IDEX(r,dec),IDEX(r,dec),IDEX(r,dec),
+  /* 0x4c */	IDEX(r,dec),IDEX(r,dec),IDEX(r,dec),IDEX(r,dec),
+  /* 0x50 */	IDEX(r,push), IDEX(r,push), IDEX(r,push), IDEX(r,push),//实现push,GPR
+  /* 0x54 */	IDEX(r,push), IDEX(r,push), IDEX(r,push), IDEX(r,push),//实现push,GPR
   /* 0x58 */	IDEX(r,pop), IDEX(r,pop), IDEX(r,pop), IDEX(r,pop),
-  /* 0x5c */	EMPTY, IDEX(r,pop), IDEX(r,pop), IDEX(r,pop),
+  /* 0x5c */	IDEX(r,pop), IDEX(r,pop), IDEX(r,pop), IDEX(r,pop),
   /* 0x60 */	EX(pusha), EX(popa), EMPTY, EMPTY,
   /* 0x64 */	EMPTY, EMPTY, EX(operand_size), EMPTY,
-  /* 0x68 */	IDEX(I,push), EMPTY, IDEXW(push_SI,push,1), IDEX(I_E2G,imul3),
+  /* 0x68 */	IDEX(push_SI,push), EMPTY, IDEXW(push_SI,push,1), EMPTY,
   /* 0x6c */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x70 */	IDEXW(J,jcc,1), IDEXW(J,jcc,1), IDEXW(J,jcc,1), IDEXW(J,jcc,1),
   /* 0x74 */	IDEXW(J,jcc,1), IDEXW(J,jcc,1), IDEXW(J,jcc,1), IDEXW(J,jcc,1),
   /* 0x78 */	IDEXW(J,jcc,1), IDEXW(J,jcc,1), IDEXW(J,jcc,1), IDEXW(J,jcc,1),
   /* 0x7c */	IDEXW(J,jcc,1), IDEXW(J,jcc,1), IDEXW(J,jcc,1), IDEXW(J,jcc,1),
-  /* 0x80 */	IDEXW(I2E, gp1, 1), IDEX(I2E, gp1), EMPTY, IDEX(SI2E, gp1),
+  /* 0x80 */	IDEXW(I2E, gp1, 1), IDEX(I2E, gp1), EMPTY, IDEX(SI2E, gp1),//实现sub，83 ec。ec:1110 1100。中间三位是101，为5，故EX(sub)填入gp1[5]
   /* 0x84 */	IDEXW(G2E,test,1), IDEX(G2E,test), EMPTY, EMPTY,
   /* 0x88 */	IDEXW(mov_G2E, mov, 1), IDEX(mov_G2E, mov), IDEXW(mov_E2G, mov, 1), IDEX(mov_E2G, mov),
   /* 0x8c */	EMPTY, IDEX(lea_M2G,lea), EMPTY, EMPTY,
@@ -114,23 +120,23 @@ opcode_entry opcode_table [512] = {
   /* 0x9c */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xa0 */	IDEXW(O2a, mov, 1), IDEX(O2a, mov), IDEXW(a2O, mov, 1), IDEX(a2O, mov),
   /* 0xa4 */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0xa8 */	IDEXW(I2a,test,1), IDEX(I2a,test), EMPTY, EMPTY,
+  /* 0xa8 */	IDEXW(I2a,test,1), IDEX(I2r,test),EMPTY, EMPTY,
   /* 0xac */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xb0 */	IDEXW(mov_I2r, mov, 1), IDEXW(mov_I2r, mov, 1), IDEXW(mov_I2r, mov, 1), IDEXW(mov_I2r, mov, 1),
   /* 0xb4 */	IDEXW(mov_I2r, mov, 1), IDEXW(mov_I2r, mov, 1), IDEXW(mov_I2r, mov, 1), IDEXW(mov_I2r, mov, 1),
   /* 0xb8 */	IDEX(mov_I2r, mov), IDEX(mov_I2r, mov), IDEX(mov_I2r, mov), IDEX(mov_I2r, mov),
   /* 0xbc */	IDEX(mov_I2r, mov), IDEX(mov_I2r, mov), IDEX(mov_I2r, mov), IDEX(mov_I2r, mov),
-  /* 0xc0 */	IDEXW(gp2_Ib2E, gp2, 1), IDEX(gp2_Ib2E, gp2), EMPTY, EX(ret),
+  /* 0xc0 */	IDEXW(gp2_Ib2E, gp2, 1), IDEX(gp2_Ib2E, gp2), EMPTY, EX(ret),//实现ret
   /* 0xc4 */	EMPTY, EMPTY, IDEXW(mov_I2E, mov, 1), IDEX(mov_I2E, mov),
   /* 0xc8 */	EMPTY, EX(leave), EMPTY, EMPTY,
-  /* 0xcc */	EX(int), IDEXW(I,int,1), EMPTY, EX(iret),
+  /* 0xcc */	EMPTY, IDEXW(I, int, 1), EMPTY, EX(iret),
   /* 0xd0 */	IDEXW(gp2_1_E, gp2, 1), IDEX(gp2_1_E, gp2), IDEXW(gp2_cl2E, gp2, 1), IDEX(gp2_cl2E, gp2),
   /* 0xd4 */	EMPTY, EMPTY, EX(nemu_trap), EMPTY,
   /* 0xd8 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xdc */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0xe0 */	EMPTY, EMPTY, EMPTY, IDEXW(J,jcc,1),
+  /* 0xe0 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xe4 */	IDEXW(in_I2a,in,1), IDEX(in_I2a,in), IDEXW(out_a2I,out,1), IDEX(out_a2I,out),
-  /* 0xe8 */	IDEX(J,call), IDEX(J,jmp), EMPTY, IDEXW(J,jmp,1),
+  /* 0xe8 */	IDEX(J,call), IDEX(J,jmp), EMPTY, IDEXW(J,jmp,1),//实现call
   /* 0xec */	IDEXW(in_dx2a,in,1), IDEX(in_dx2a,in), IDEXW(out_a2dx,out,1), IDEX(out_a2dx,out),
   /* 0xf0 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xf4 */	EMPTY, EMPTY, IDEXW(E, gp3, 1), IDEX(E, gp3),
@@ -149,8 +155,8 @@ opcode_entry opcode_table [512] = {
   /* 0x1c */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x20 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x24 */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0x28 */	EMPTY, IDEX(G2E,sub), EMPTY, IDEX(G2E,sub),
-  /* 0x2c */	EMPTY, IDEX(I2a,sub), EMPTY, EMPTY,
+  /* 0x28 */	EMPTY, EMPTY, EMPTY, EMPTY,
+  /* 0x2c */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x30 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x34 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x38 */	EMPTY, EMPTY, EMPTY, EMPTY,
@@ -171,22 +177,22 @@ opcode_entry opcode_table [512] = {
   /* 0x74 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x78 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x7c */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0x80 */	IDEX(J,jcc), IDEX(J,jcc), IDEX(J,jcc), IDEX(J,jcc),
-  /* 0x84 */	IDEX(J,jcc), IDEX(J,jcc), IDEX(J,jcc), IDEX(J,jcc),
-  /* 0x88 */	IDEX(J,jcc), IDEX(J,jcc), IDEX(J,jcc), IDEX(J,jcc),
-  /* 0x8c */	IDEX(J,jcc), IDEX(J,jcc), IDEX(J,jcc), IDEX(J,jcc),
-  /* 0x90 */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0x94 */	IDEXW(E,setcc,1), IDEXW(E,setcc,1), EMPTY, EMPTY,
-  /* 0x98 */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0x9c */	EMPTY, EMPTY, EMPTY, IDEXW(E,setcc,1),
+  /* 0x80 */	IDEX(J,jcc), IDEX(J,jcc), IDEX(J,jcc), IDEX(J,jcc), 
+  /* 0x84 */	IDEX(J,jcc), IDEX(J,jcc), IDEX(J,jcc), IDEX(J,jcc), 
+  /* 0x88 */	IDEX(J,jcc), IDEX(J,jcc), IDEX(J,jcc), IDEX(J,jcc), 
+  /* 0x8c */	IDEX(J,jcc), IDEX(J,jcc), IDEX(J,jcc), IDEX(J,jcc), 
+  /* 0x90 */	IDEXW(E,setcc,1), IDEXW(E,setcc,1), IDEXW(E,setcc,1), IDEXW(E,setcc,1),
+  /* 0x94 */	IDEXW(E,setcc,1), IDEXW(E,setcc,1), IDEXW(E,setcc,1), IDEXW(E,setcc,1),
+  /* 0x98 */	IDEXW(E,setcc,1), IDEXW(E,setcc,1), IDEXW(E,setcc,1), IDEXW(E,setcc,1),
+  /* 0x9c */	IDEXW(E,setcc,1), IDEXW(E,setcc,1), IDEXW(E,setcc,1), IDEXW(E,setcc,1),
   /* 0xa0 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xa4 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xa8 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xac */	EMPTY, EMPTY, EMPTY, IDEX(E2G,imul2),
   /* 0xb0 */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0xb4 */	EMPTY, EMPTY, IDEXW(mov_E2G,movzx,1), IDEXW(mov_E2G,movzx,2),
+  /* 0xb4 */	EMPTY, EMPTY, IDEXW(E2G,movzx,1), IDEXW(E2G,movzx,2),
   /* 0xb8 */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0xbc */	EMPTY, EMPTY, IDEXW(mov_E2G,movsx,1), IDEXW(mov_E2G,movsx,2),
+  /* 0xbc */	EMPTY, EMPTY, IDEXW(E2G,movsx,1), IDEXW(E2G,movsx,2),
   /* 0xc0 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xc4 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xc8 */	EMPTY, EMPTY, EMPTY, EMPTY,
@@ -206,31 +212,35 @@ opcode_entry opcode_table [512] = {
 };
 
 static make_EHelper(2byte_esc) {
-  uint32_t opcode = instr_fetch(eip, 1) | 0x100;
+  uint32_t opcode = instr_fetch(eip, 1) | 0x100;//用两个字节来确定指令格式！
   decoding.opcode = opcode;
   set_width(opcode_table[opcode].width);
   idex(eip, &opcode_table[opcode]);
 }
 
 make_EHelper(real) {
-  uint32_t opcode = instr_fetch(eip, 1);
-  decoding.opcode = opcode;
-  set_width(opcode_table[opcode].width);
-  idex(eip, &opcode_table[opcode]);
+  uint32_t opcode = instr_fetch(eip, 1);//取指，得到指令的第一个字节，将其解释成opcode
+  //Why 第一个字节能确定？opcode_table提供了512个指令位
+  decoding.opcode = opcode;//将opcode记录在全局译码信息decoding中
+  set_width(opcode_table[opcode].width);//查表获取操作数宽度信息，通过set_width函数将其记录在全局译码信息decoding中
+  idex(eip, &opcode_table[opcode]);//对指令进行进一步的译码与执行
 }
 
-static inline void update_eip(void) {
+//更新eip
+static inline void update_eip(void) {//如果要跳转(decoing.is_jmp=1)，首先初始化跳转指令，再跳(eip=decoing.jmp_eip)；
+                                     //否则顺序执行(eip=decoing.seq_eip)
   cpu.eip = (decoding.is_jmp ? (decoding.is_jmp = 0, decoding.jmp_eip) : decoding.seq_eip);
 }
 
+//***exec current eip instruction and then update eip
 void exec_wrapper(bool print_flag) {
 #ifdef DEBUG
   decoding.p = decoding.asm_buf;
   decoding.p += sprintf(decoding.p, "%8x:   ", cpu.eip);
 #endif
 
-  decoding.seq_eip = cpu.eip;
-  exec_real(&decoding.seq_eip);
+  decoding.seq_eip = cpu.eip;//保存当前的%eip到全局译码信息decoding的成员seq_eip中
+  exec_real(&decoding.seq_eip);//将其地址作为参数传入exec_real函数中，进行取指、译码、执行
 
 #ifdef DEBUG
   int instr_len = decoding.seq_eip - cpu.eip;
@@ -246,8 +256,9 @@ void exec_wrapper(bool print_flag) {
   uint32_t eip = cpu.eip;
 #endif
 
-  update_eip();
+  update_eip();//更新eip
 
+//实现NEMU和QEMU的状态对比
 #ifdef DIFF_TEST
   void difftest_step(uint32_t);
   difftest_step(eip);
