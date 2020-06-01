d46b262fa769f651104d2ecae4c0c61273ab7f72
tracer-ics2017 <tracer@njuics.org>
2020-06-02 00:33:11 +0800

    >  compile 1410576 llt Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 i686 i686 GNU/Linux  00:33:11 up  3:06,  2 users,  load average: 0.00, 0.02, 0.02 f8ad1b3341fc382fb1ef637897a876c2e6261bf3

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -2,74 +2,75 @@
 
 make_EHelper(test) {
   //TODO();
-  rtl_and(&t2, &id_dest->val, &id_src->val);//利用rtl基本操作进行运算
-  //更新各个标志位
-  rtl_update_ZFSF(&t2, id_dest->width);
-  rtl_set_OF(&tzero);
+  rtl_and(&t2,&id_dest->val,&id_src->val);
+ 
+  rtl_update_ZFSF(&t2,id_dest->width);
   rtl_set_CF(&tzero);
+  rtl_set_OF(&tzero);
   print_asm_template2(test);
 }
 
 make_EHelper(and) {
   //TODO();
-  rtl_and(&t2, &id_dest->val, &id_src->val);//利用rtl基本操作进行运算
-  operand_write(id_dest, &t2);//完成计算，写入结果
-  //更新各个标志位
-  rtl_update_ZFSF(&t2, id_dest->width);
-  rtl_set_OF(&tzero);
+  rtl_and(&t2,&id_dest->val,&id_src->val);
+  operand_write(id_dest,&t2);
+  rtl_update_ZFSF(&t2,id_dest->width);
   rtl_set_CF(&tzero);
+  rtl_set_OF(&tzero);
+
   print_asm_template2(and);
 }
 
 make_EHelper(xor) {
   //TODO();
-  rtl_xor(&t2, &id_dest->val, &id_src->val);//利用rtl基本操作进行运算
-  operand_write(id_dest, &t2);//完成计算，写入结果
-  //更新各个标志位
-  rtl_update_ZFSF(&t2, id_dest->width);
-  rtl_set_OF(&tzero);
+  rtl_xor(&t2,&id_dest->val,&id_src->val);
+  operand_write(id_dest,&t2);
+
+  rtl_update_ZFSF(&t2,id_dest->width);
   rtl_set_CF(&tzero);
+  rtl_set_OF(&tzero);
+
   print_asm_template2(xor);
 }
 
 make_EHelper(or) {
   //TODO();
-  rtl_or(&t2, &id_dest->val, &id_src->val);//利用rtl基本操作进行运算
-  operand_write(id_dest, &t2);//完成计算，写入结果
-  //更新各个标志位
-  rtl_update_ZFSF(&t2, id_dest->width);
-  rtl_set_OF(&tzero);
+  rtl_or(&t2,&id_dest->val,&id_src->val);
+  operand_write(id_dest,&t2);
+
+  rtl_update_ZFSF(&t2,id_dest->width);
   rtl_set_CF(&tzero);
+  rtl_set_OF(&tzero);
+
   print_asm_template2(or);
 }
 
 make_EHelper(sar) {
   //TODO();
   // unnecessary to update CF and OF in NEMU
-  rtl_sar(&t2, &id_dest->val, &id_src->val);//利用rtl基本操作进行运算
-  operand_write(id_dest, &t2);//完成计算，写入结果
-  //更新ZF和SF
-  rtl_update_ZFSF(&t2, id_dest->width);
+  rtl_sext(&t2,&id_dest->val,id_dest->width);
+  //rtl_sar(&t2,&id_dest->val,&id_src->val);
+  rtl_sar(&t2,&t2,&id_src->val);
+  operand_write(id_dest,&t2);
+  rtl_update_ZFSF(&t2,id_dest->width);
   print_asm_template2(sar);
 }
 
 make_EHelper(shl) {
   //TODO();
   // unnecessary to update CF and OF in NEMU
-  rtl_shl(&t2, &id_dest->val, &id_src->val);//利用rtl基本操作进行运算
-  operand_write(id_dest, &t2);//完成计算，写入结果
-  //更新ZF和SF
-  rtl_update_ZFSF(&t2, id_dest->width);
+  rtl_shl(&t2,&id_dest->val,&id_src->val);
+  operand_write(id_dest,&t2);
+  rtl_update_ZFSF(&t2,id_dest->width);
   print_asm_template2(shl);
 }
 
 make_EHelper(shr) {
   //TODO();
   // unnecessary to update CF and OF in NEMU
-  rtl_shr(&t2, &id_dest->val, &id_src->val);//利用rtl基本操作进行运算
-  operand_write(id_dest, &t2);//完成计算，写入结果
-  //更新ZF和SF
-  rtl_update_ZFSF(&t2, id_dest->width);
+  rtl_shr(&t2,&id_dest->val,&id_src->val);
+  operand_write(id_dest,&t2);
+  rtl_update_ZFSF(&t2,id_dest->width);
   print_asm_template2(shr);
 }
 
@@ -83,32 +84,32 @@ make_EHelper(setcc) {
 
 make_EHelper(not) {
   //TODO();
-  rtl_mv(&t2,&id_dest->val);
+  rtl_li(&t2,id_dest->val);
   rtl_not(&t2);
   operand_write(id_dest,&t2);
   print_asm_template1(not);
 }
 
-//原框架中没有该指令，自己添加
 make_EHelper(rol) {
-  rtl_shl(&t0,&id_dest->val,&id_src->val);//左移n
-  rtl_shri(&t1,&id_dest->val,id_dest->width*8-id_src->val);//对于最左边的位，左移n相当于右移width-n
-  rtl_or(&t0,&t1,&t0);
-  operand_write(id_dest, &t0);
-
-  //注意，下面的部分按照手册P372来
-  rtl_get_CF(&t2);//得到CF位
-  t0 = id_src->val; //count
-  if(t0 == 1)
-  {
-      rtl_msb(&t1,&id_dest->val,id_dest->width);//Get high-order bit of r/m
-      if(t2!=t1)
-      {
-          rtl_set_OF(&t0);
-      }
-      else{
-          rtl_set_OF(&tzero);
-      }
+  for(t0=0;t0<id_src->val;t0++){
+    rtl_shri(&t1,&id_dest->val,id_dest->width*8-1);
+    rtl_shli(&t2,&id_dest->val,1);
+    rtl_or(&t2,&t2,&t1);
+    rtl_mv(&id_dest->val,&t2);
   }
+  operand_write(id_dest,&t2);
+  if(id_src->val==1)
+  {
+    rtl_msb(&t1,&id_dest->val,id_dest->width);
+    rtl_get_CF(&t2);
+    if(t2!=t1){
+      rtl_set_OF(&id_src->val);
+    }
+    else{
+      rtl_set_OF(&tzero);
+    }
+  }
+
   print_asm_template2(rol);
 }
+
