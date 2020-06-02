d5eccd8db34e3130f3ef4a7a783240c4237819a1
tracer-ics2017 <tracer@njuics.org>
2020-06-02 22:13:06 +0800

    >  run 1410576 llt Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 i686 i686 GNU/Linux  22:13:06 up 12:59,  1 user,  load average: 0.08, 0.04, 0.01 6eec5dfdd11d9a9b14bc82ae8abb5c6a99ed02bd

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -39,6 +39,7 @@ uintptr_t loader(_Protect *as, const char *filename) {
     //将虚拟页映射到获取到的物理页
     Log("Map va:0x%08x to pa:0x%08x",va,pa);
     _map(as,va,pa);
+    Log("PDT_Base:0x%08x",as->ptr);
     //读取文件，读取长度不能超过页的大小
     fs_read(fd,pa,(pa-va)<PGSIZE?(end-va):PGSIZE);
   }
