9a598ebbde9728b459f959fc1203f4eaf2916511
tracer-ics2017 <tracer@njuics.org>
2020-06-02 17:53:42 +0800

    >  compile 1410576 llt Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 i686 i686 GNU/Linux  17:53:42 up  8:39,  1 user,  load average: 0.00, 0.00, 0.00 1d217cca8bd08dce61ecaeb22c432362b8c8c7

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -14,91 +14,98 @@ uint8_t pmem[PMEM_SIZE];
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
-  int mmioFlag=is_mmio(addr);
-  if(mmioFlag != -1){
-    return mmio_read(addr,len,mmioFlag);
+  int mmio= is_mmio(addr);
+  if(mmio != -1){
+	return mmio_read(addr, len, mmio);
   }
-  else{
-    return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
-  }
-
+  return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
 }
 
 void paddr_write(paddr_t addr, int len, uint32_t data) {
-  int mmioFlag=is_mmio(addr);
-  if(mmioFlag != -1){
-    mmio_write(addr,len,data,mmioFlag);
+  int mmio= is_mmio(addr);
+  if(mmio != -1){
+	return mmio_write(addr, len, data, mmio);
   }
-  else{
-     memcpy(guest_to_host(addr), &data, len); 
-  }
+  memcpy(guest_to_host(addr), &data, len);
 }
 
-paddr_t page_translate(paddr_t addr, bool iswrite){
-  PDE pde;
-  PTE pte;
+paddr_t page_translate(vaddr_t vaddr, bool dirty){
+	if(cpu.cr0.paging!=1){
+		return vaddr;
+	}
+	else{
+		paddr_t basePDE = cpu.cr3.val;
+		paddr_t addrPDE = basePDE + (vaddr>>22) * 4;
+		paddr_t pde = paddr_read(addrPDE, 4);
+		assert(pde & 0x1);
 
-  paddr_t dir = (addr >> 22) & 0x3ff;
-  paddr_t page = (addr >> 12) & 0x3ff;
-  paddr_t offset = addr & 0xfff;
-  if(cpu.cr0.paging){
+		paddr_t basePTE = pde & 0xfffff000;
+		paddr_t addrPTE = basePTE + ((vaddr>>12) & 0x3ff) * 4;
+		paddr_t pte = paddr_read(addrPTE, 4);
+		assert(pte & 0x1);
 
-    uint32_t pdb = cpu.cr3.page_directory_base;
-    pde.val = paddr_read((pdb << 12) | (dir << 2), 4);
-    if (!pde.present) panic("Invalid page directory entry at address %#x\n", addr);
-    if (!pde.accessed)
-    {
-      pde.accessed = 1;
-      paddr_write((pdb << 12) | (dir << 2), 4, pde.val);
-    }
-    
-    pte.val = paddr_read((pde.val & 0xfffff000) | (page << 2), 4);
-    if (! pte.present) panic("Invalid page table entry at address %#x\n", addr);
-    if (! pte.accessed) pte.accessed = 1;
-    if (iswrite) pte.dirty = 1;
-    paddr_write((pde.val & 0xfffff000) | (page << 2), 4, pte.val);
-    return (pte.val & 0xfffff000) | offset;
-  }
-  return addr;
+		paddr_t basePage = pte & 0xfffff000;
+		paddr_t page = basePage + (vaddr & 0xfff);
+		
+		if(dirty){
+			pde |= 0x60;
+			pte |= 0x60;
+		}
+		else{
+			pde |= 0x20;
+			pte |= 0x20;	
+		}
+		paddr_write(addrPDE, 4, pde);
+		paddr_write(addrPTE, 4, pte);
+
+		return page;
+	}
 }
-
-
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
-  if ((addr & PAGE_MASK) + len > PAGE_SIZE) {
-    //assert(0);
-    //Log("read");
-    int len1,len2;
-    uint32_t data;
-    paddr_t paddr;
-    len1 = PAGE_SIZE - (addr & PAGE_MASK);
-    len2 = len - len1;
-    paddr = page_translate(addr,false);
-    data = paddr_read(paddr,len1);
-    paddr = page_translate(addr + len1,false);
-    data = (paddr_read(paddr,len2) << (len1 << 3)) | data;
-    return data;
-  }
-  if(cpu.cr0.paging) {
-    //Log("vaddr:0x%08x,paddr:0x%08x",addr,page_translate(addr,false));
-    return paddr_read(page_translate(addr,false), len);
-    }
-  else return paddr_read(addr, len);
+	if((addr & 0xfff) + len > 0x1000){
+		paddr_t paddr = page_translate(addr, false);
+		uint32_t low = paddr_read(
+			paddr, 
+			(int)(0x1000-(addr & 0xfff)));
+
+		paddr = page_translate(
+			addr + (int)(0x1000-(addr & 0xfff)), 
+			false);
+		uint32_t high = paddr_read(
+			paddr, 
+			len - (int)(0x1000-(addr & 0xfff)));
+		
+		return (high << ((0x1000 - (addr & 0xfff)) * 8)) + low;
+	}
+	else{
+		paddr_t paddr = page_translate(addr, false);
+		return paddr_read(paddr, len);
+	}
 }
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
-  if ((addr & PAGE_MASK) + len > PAGE_SIZE) {
-    //Log("write");
-    int len1,len2;
-    paddr_t paddr;
-    len1 = PAGE_SIZE - (addr & PAGE_MASK);
-    len2 = len - len1;
-    paddr = page_translate(addr,true);
-    paddr_write(paddr,len1,data);
-    paddr = page_translate(addr + len1,true);
-    paddr_write(paddr,len2,data >> (len1<<3));
-   // assert(0);
-  }
-  if(cpu.cr0.paging) return paddr_write(page_translate(addr,true), len, data);
-  else return paddr_write(addr, len, data);
+	if((addr & 0xfff) + len > 0x1000){
+		uint32_t high = data >> ((0x1000 - (addr & 0xfff)) * 8);
+		uint32_t low = 
+			data & ((1<<((0x1000-(addr & 0xfff))*8))-1);		
+	
+		paddr_t paddr = page_translate(addr, true);
+		paddr_write(
+			paddr,
+			0x1000-(addr & 0xfff), 
+			low);
+
+		paddr = page_translate(
+			addr + 0x1000-(addr & 0xfff), 
+			true);
+		paddr_write(
+			paddr,
+			len - (int)(0x1000-(addr & 0xfff)), 
+			high);
+	}
+	else{
+		paddr_t paddr = page_translate(addr, true);
+		paddr_write(paddr, len, data);
+	}
 }
