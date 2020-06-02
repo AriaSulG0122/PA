e48d787d0f6cc47c85680cbafba394144a2c2da0
tracer-ics2017 <tracer@njuics.org>
2020-06-02 16:19:35 +0800

    >  run 1410576 llt Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 i686 i686 GNU/Linux  16:19:35 up  7:39,  2 users,  load average: 0.00, 0.00, 0.00 c66a8250322d1d698d61bd3765282a0ee515db14

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -16,6 +16,7 @@ void free_page(void *p) {
 
 /* The brk() system call handler. */
 int mm_brk(uint32_t new_brk) {
+  /*
   if(current->cur_brk==0){
     current->cur_brk=current->max_brk=new_brk;
   }else{
@@ -28,7 +29,7 @@ int mm_brk(uint32_t new_brk) {
       current->max_brk=va;
     }
     current->cur_brk=new_brk;
-  }
+  }*/
   return 0;
 }
 
