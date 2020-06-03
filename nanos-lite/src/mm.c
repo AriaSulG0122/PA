f17c578026c0972013a268f0990e50f47b472d96
tracer-ics2017 <tracer@njuics.org>
2020-06-03 10:57:59 +0800

    >  run 1410576 llt Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 i686 i686 GNU/Linux  10:57:59 up 15:57,  1 user,  load average: 0.08, 0.02, 0.01 e4f146956ebb3f7596023ea3d359ffba5f955445

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -3,38 +3,47 @@
 
 static void *pf = NULL;
 
-void* new_page(void) {
+void *new_page(void)
+{
   assert(pf < (void *)_heap.end);
   void *p = pf;
   pf += PGSIZE;
   return p;
 }
 
-void free_page(void *p) {
+void free_page(void *p)
+{
   panic("not implement yet");
 }
 
 /* The brk() system call handler. */
-int mm_brk(uint32_t new_brk) {
-  /*
-  if(current->cur_brk==0){
-    current->cur_brk=current->max_brk=new_brk;
-  }else{
-    if(new_brk>current->max_brk){
-      uintptr_t pa,va;
-      for(va=(current->max_brk+0xfff)&~0xfff;va<new_brk;va+=PGSIZE){
-        pa=(uintptr_t)new_page();
-        _map(&current->as,(void*)va,(void*)pa);
+int mm_brk(uint32_t new_brk)
+{
+
+  if (current->cur_brk == 0)
+  {
+    current->cur_brk = current->max_brk = new_brk;
+  }
+  else
+  {
+    if (new_brk > current->max_brk)
+    {//将[current->max_brk,new_brk)的空间映射到地址空间current->as
+      uintptr_t pa, va;
+      for (va = (current->max_brk + 0xfff) & ~0xfff; va < new_brk; va += PGSIZE)
+      {
+        pa = (uintptr_t)new_page();
+        _map(&current->as, (void *)va, (void *)pa);
       }
-      current->max_brk=va;
+      current->max_brk = new_brk;
     }
-    current->cur_brk=new_brk;
-  }*/
+    current->cur_brk = new_brk;
+  }
   return 0;
 }
 
 //初始化MM，MM是存储管理器(Memory Manager)模块，专门负责分页相关的存储管理
-void init_mm() {
+void init_mm()
+{
   //将TRM提供的堆区起始地址作为空闲物理页的首地址
   //将来会通过new_page()函数来分配空闲的物理页
   pf = (void *)PGROUNDUP((uintptr_t)_heap.start);
