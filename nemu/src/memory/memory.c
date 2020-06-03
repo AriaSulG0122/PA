72dd7baffa0600c510abce87bf2a015967090a01
tracer-ics2017 <tracer@njuics.org>
2020-06-03 11:35:09 +0800

    >  compile 1410576 llt Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:44:23 UTC 2020 i686 i686 i686 GNU/Linux  11:35:09 up 16:34,  1 user,  load average: 0.00, 0.00, 0.00 bf33f0653abb5958cebefeda482ce7245b04bc02

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -55,7 +55,7 @@ paddr_t page_translate(vaddr_t addr, bool is_write)
   pde.val = paddr_read((PDT_base << 12) + (dir << 2), 4);
   if (!pde.present)
   { //检查p位
-    Log("page_translate: addr: 0x%x\n", addr);
+    Log("page_translate: addr: 0x%x\n,pde.val:0x%08x", addr,pde.val);
     Log("page_translate: dir: 0x%x page: 0x%x offset: 0x%x PDT_base: 0x%x\n", dir, page, offset, PDT_base);
     assert(pde.present);
   }
